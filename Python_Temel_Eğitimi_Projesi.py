8cbe0abcedff5a67d4b788d3443b4081f1eff4de
Hasan FIRAT <hsn63frt@gmail.com>
2022-10-31 13:30:42 +0300

    Python doyasında ikinci örnek yapıldı.

--- Python_Temel_Eğitimi_Projesi.py
+++ Python_Temel_Eğitimi_Projesi.py
@@ -10,3 +10,17 @@ l=[[1,'a',['cat'],2],[[[3]],'dog'],4,5]
 o=[]
 flatten_list(l, o)
 print(o)
+
+def ters_list(in_list, out_list):
+    for i in in_list:
+        if type(i) is list:
+            i=i[::-1]
+            out_list.append(i)
+        else:
+            out_list.append(i)
+
+liste=[[1, 2], [3, 4], [5, 6, 7]]
+ters_liste=[]
+ters_list(liste, ters_liste)
+ters_liste=ters_liste[::-1]
+print(ters_liste)
